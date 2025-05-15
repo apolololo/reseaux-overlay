372e351841e8cf82324f3bda7975056d09dad091
apolololo <140184462+apolololo@users.noreply.github.com>
2025-05-15 20:23:19 +0200

    g

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const PROD_URL = 'https://apo-overlay.netlify.app';
 export const config = {
   apiUrl: PROD_URL,
   appUrl: PROD_URL,
-  twitchClientId: '6qdvxjxzwxjvxlm5ydkw9f6jg3xdw1',
+  twitchClientId: 'gp762nuuoqcoxypju8c569th9wz7q5',
   twitchRedirectUri: `${PROD_URL}/auth/callback`,
   twitchScopes: ['user:read:email', 'channel:read:subscriptions']
 }; 
