652aa406d23d51cba04c4002deeaa417ab885bbf
apolololo <140184462+apolololo@users.noreply.github.com>
2025-05-21 22:11:38 +0200

    fvdg

--- src/overlays/starting/background.js
+++
@@ -1,7 +0,0 @@
-// Optimisation : Nous avons complètement éliminé les animations JavaScript pour le fond
-// Toutes les animations sont maintenant gérées en CSS pour de meilleures performances
-// Ce fichier est conservé pour référence mais n'est plus utilisé activement
-
-// Note: Si vous souhaitez ajouter des animations JavaScript à l'avenir,
-// assurez-vous d'utiliser requestAnimationFrame et de limiter les calculs
-// pour maintenir de bonnes performances
