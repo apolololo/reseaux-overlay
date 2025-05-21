a57516ed11c703463dd976bf7d2f1fb3fae626c7
apolololo <140184462+apolololo@users.noreply.github.com>
2025-05-21 22:52:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/',
@@ -8,16 +7,8 @@ export default defineConfig({
       input: {
         main: 'index.html',
         auth: 'auth/auth.html',
-        callback: 'auth/callback.html',
-        overlay: resolve(__dirname, 'overlay.html'), // Added this entry
-        apo: resolve(__dirname, 'src/overlays/apo/overlay.html'),
-        starting: resolve(__dirname, 'src/overlays/starting/overlay.html'),
-        brb: resolve(__dirname, 'src/overlays/brb/overlay.html'),
-        brbVideo: resolve(__dirname, 'src/overlays/brb/overlay-video.html'),
-        end: resolve(__dirname, 'src/overlays/end/overlay.html'),
-        gameStatus: resolve(__dirname, 'src/overlays/game-status/overlay.html'),
-        mapInfo: resolve(__dirname, 'src/overlays/maps/overlay.html')
-      },
+        callback: 'auth/callback.html'
+      }
     },
     assetsDir: 'assets',
     outDir: 'dist',
