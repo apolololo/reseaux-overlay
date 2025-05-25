edb9bcecfced3f989a67ad873891828f129772fc
apolololo <140184462+apolololo@users.noreply.github.com>
2025-05-25 15:29:04 +0200

    aaaaaaaaaa

--- vite.config.js
+++ vite.config.js
@@ -13,15 +13,20 @@ export default defineConfig({
         editor: resolve(__dirname, 'editor.html'),
         overlay: resolve(__dirname, 'overlay.html'),
         login: resolve(__dirname, 'login.html'),
-        navigation: resolve(__dirname, 'src/components/navigation.js'),
         'my-overlays': resolve(__dirname, 'my-overlays.html'),
         marketplace: resolve(__dirname, 'marketplace.html'),
-
-      }
+      },
+      external: [
+        'src/components/navigation.js',
+        'src/templates/default/script.js'
+      ]
     }
   },
   server: {
     port: 8080,
     open: true
+  },
+  optimizeDeps: {
+    include: []
   }
 });
